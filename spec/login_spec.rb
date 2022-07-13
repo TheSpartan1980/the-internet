6517935f74e7610337c1e8f59a532154b6010aac
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2022-07-13 20:21:32 +0100

    Made examples more descriptive

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -6,12 +6,12 @@ describe 'Login' do
     @login = Login.new(@driver)
   end
 
-  it 'succeeded' do
+  it 'Example 1: Successfully logged in' do
     @login.with('tomsmith', 'SuperSecretPassword!')
     expect(@login.success_message_present?).to eql true
   end
 
-  it 'failed' do
+  it 'Example 2: Failed attempt to log in' do
     @login.with('asdf', 'asdf')
     expect(@login.failure_message_present?).to eql true
   end
